394cc4996b20fbbd845d5eef45dd8675dadac8a3
Jesustpsk <dm.kuzmin11@mail.ru>
2025-09-22 20:54:37 +0300

    Поправил миграцию и константы

--- Persistence/Constants/SqlPathsConstants.cs
+++ Persistence/Constants/SqlPathsConstants.cs
@@ -4,17 +4,17 @@ public static class SqlPathsConstants
 {
     private const string RootPath = @"Dapper\Sql";
 
-    private const string EmploeeModelPath = @"Employee";
-    private const string DepartmentModelPath = @"Employee";
-    private const string PassportModelPath = @"Employee";
+    private const string EmployeeModelPath = @"Employee";
+    private const string DepartmentModelPath = @"Department";
+    private const string PassportModelPath = @"Passport";
     
-    public const string AddEmployee = $@"{RootPath}{EmploeeModelPath}\AddEmployee.sql";
-    public const string DeleteEmployee = $@"{RootPath}{EmploeeModelPath}\DeleteEmployee.sql";
-    public const string UpdateEmployee = $@"{RootPath}{EmploeeModelPath}\UpdateEmployee.sql";
-    public const string GetEmployeeById = $@"{RootPath}{EmploeeModelPath}\GetEmployeeById.sql";
-    public const string GetEmployeeWithNestedTablesById = $@"{RootPath}{EmploeeModelPath}\GetEmployeeWithNestedTablesById.sql";
-    public const string GetAllEmployees = $@"{RootPath}{EmploeeModelPath}\GetEmployees.sql";
-    public const string GetAllEmployeesWithNestedTables = $@"{RootPath}{EmploeeModelPath}\GetEmployeesWithNestedTables.sql";
+    public const string AddEmployee = $@"{RootPath}{EmployeeModelPath}\AddEmployee.sql";
+    public const string DeleteEmployee = $@"{RootPath}{EmployeeModelPath}\DeleteEmployee.sql";
+    public const string UpdateEmployee = $@"{RootPath}{EmployeeModelPath}\UpdateEmployee.sql";
+    public const string GetEmployeeById = $@"{RootPath}{EmployeeModelPath}\GetEmployeeById.sql";
+    public const string GetEmployeeWithNestedTablesById = $@"{RootPath}{EmployeeModelPath}\GetEmployeeWithNestedTablesById.sql";
+    public const string GetAllEmployees = $@"{RootPath}{EmployeeModelPath}\GetEmployees.sql";
+    public const string GetAllEmployeesWithNestedTables = $@"{RootPath}{EmployeeModelPath}\GetEmployeesWithNestedTables.sql";
     
     public const string AddDepartment = $@"{RootPath}{DepartmentModelPath}\AddDepartment.sql";
     
--- Persistence/Migrations/InitialMigration.cs
+++ Persistence/Migrations/InitialMigration.cs
@@ -9,22 +9,6 @@ public class InitialMigration_20250922155259 : Migration
 {
     public override void Up()
     {
-        Create.Table("employees").WithDescription("Список сотрудников")
-            .WithColumn("id").AsInt64()
-                .PrimaryKey()
-                    .Identity(PostgresGenerationType.Always)
-            .WithColumn("name").AsString(50)
-            .WithColumn("surname").AsString(50)
-            .WithColumn("phone").AsString(20)
-            .WithColumn("company_id").AsInt64()
-                .NotNullable()
-            .WithColumn("passport_id").AsInt64()
-                .ForeignKey("fk_passport","passports", "id")
-                    .OnDelete(Rule.Cascade)
-            .WithColumn("department_id").AsInt64()
-                .ForeignKey("fk_department", "departments", "id")
-                    .OnDelete(Rule.Cascade);
-
         Create.Table("passports").WithDescription("Паспорта")
             .WithColumn("id")
             .AsInt64()
@@ -40,6 +24,22 @@ public class InitialMigration_20250922155259 : Migration
             .Identity(PostgresGenerationType.Always)
             .WithColumn("name").AsString(100)
             .WithColumn("phone").AsString(20);
+        
+        Create.Table("employees").WithDescription("Список сотрудников")
+            .WithColumn("id").AsInt64()
+                .PrimaryKey()
+                    .Identity(PostgresGenerationType.Always)
+            .WithColumn("name").AsString(50)
+            .WithColumn("surname").AsString(50)
+            .WithColumn("phone").AsString(20)
+            .WithColumn("company_id").AsInt64()
+                .NotNullable()
+            .WithColumn("passport_id").AsInt64()
+                .ForeignKey("fk_passport","passports", "id")
+                    .OnDelete(Rule.Cascade)
+            .WithColumn("department_id").AsInt64()
+                .ForeignKey("fk_department", "departments", "id")
+                    .OnDelete(Rule.Cascade);
     }
 
     public override void Down()
